214287dbb4848634d9c1533482814096dac0b9fc
ga58keb <federico.wyrwal@tum.de>
2021-05-13 11:33:44 +0200

    first commit

---
+++ src/hDQN/models.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Conv2D, Dense, Flatten, Concatenate, Input, AvgPool2D
+
+import numpy as np
+
+
+def print_node(x):
+    print(x)
+    return x
+
+class MetaControllerParams:
+    def __init__(self):
+        # Convolutional part config
+        self.conv_layers = 2
+        self.conv_kernel_size = 5
+        self.conv_kernels = 16
+
+        # Fully Connected config
+        self.hidden_layer_size = 256
+        self.hidden_layer_num = 3
+
+        # Training Params
+        self.learning_rate = 3e-5
+        self.alpha = 0.005
+        self.gamma = 0.95
+
+        # Exploration strategy
+        self.soft_max_scaling = 0.1
+
+class ControllerParams:
+    def __init__(self):
+        # Convolutional part config
+        self.conv_layers = 2
+        self.conv_kernel_size = 5
+        self.conv_kernels = 16
+
+        # Fully Connected config
+        self.hidden_layer_size = 256
+        self.hidden_layer_num = 3
+
+        # Training Params
+        self.learning_rate = 3e-5
+        self.alpha = 0.005
+        self.gamma = 0.95
+
+        # Exploration strategy
+        self.soft_max_scaling = 0.1
+
+class MetaController():

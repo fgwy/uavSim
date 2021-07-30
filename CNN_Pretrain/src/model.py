7d7633963e39d6df038e6e4bd8b6d60c70d9b6f7
ga58keb <federico.wyrwal@tum.de>
2021-07-30 15:40:50 +0200

    added Autoencoder features and changed inputs for fr1

--- CNN_Pretrain/src/model.py
+++ CNN_Pretrain/src/model.py
@@ -4,10 +4,10 @@ print(tf.__version__)
 
 class Autoencoder_flex(tf.keras.models.Model):
 
-    def __init__(self, num_layers, input_shape):
+    def __init__(self, num_layers, inp_shape):
         super(Autoencoder_flex, self).__init__()
         self.num_layers = num_layers
-        self.input_shape = input_shape
+        self.inp_shape = inp_shape
         self.encoder = self.build_encoder()
         self.decoder = self.build_decoder()
 
@@ -22,17 +22,18 @@ class Autoencoder_flex(tf.keras.models.Model):
 
     def build_encoder(self):
         encoder = tf.keras.Sequential(name='encoder')
-        encoder.add(tf.keras.layers.Input(shape=self.input_shape))
+        encoder.add(tf.keras.layers.Input(shape=self.inp_shape))
         for k in range(self.num_layers):
-            encoder.add(tf.keras.layers.Conv2D(filters=(4+4*k), kernel_size=(4, 4), name='{}th-encoding_layer'.format(k), padding='same', activation="elu"))
-            encoder.add(tf.keras.layers.MaxPooling2D((2, 2), strides=2))
+            encoder.add(tf.keras.layers.Conv2D(filters=(4+4*k), kernel_size=3, name='{}th-encoding_layer'.format(k), padding='valid', activation="elu"))
+            encoder.add(tf.keras.layers.MaxPooling2D((2, 2), strides=1))
         return encoder
 
     def build_decoder(self):
         decoder = tf.keras.Sequential(name='decoder')
-        for k in range(self.num_layers):
-            decoder.add(tf.keras.layers.Conv2DTranspose(filters=(4+4*(self.num_layers-k)), kernel_size=(4, 4), name='{}th-decoding_layer'.format(k), activation='elu', padding='same'))
+        for k in range(self.num_layers-1):
+            decoder.add(tf.keras.layers.Conv2DTranspose(filters=(4+4*(self.num_layers-k)), kernel_size=3, name='{}th-decoding_layer'.format(k), activation='elu', padding='valid'))
             decoder.add(tf.keras.layers.UpSampling2D((2, 2)))
+        decoder.add(tf.keras.layers.Conv2DTranspose(filters=self.inp_shape[2], kernel_size=3))
 
         return decoder
 
@@ -51,7 +52,7 @@ class Autoencoder(tf.keras.models.Model):
         self.decoder = tf.keras.Sequential([
             tf.keras.layers.Conv2DTranspose(8, kernel_size=3, strides=2, activation='relu', padding='same'),
             tf.keras.layers.Conv2DTranspose(16, kernel_size=3, strides=2, activation='relu', padding='same'),
-            tf.keras.layers.Conv2D(1, kernel_size=(3, 3), activation='sigmoid', padding='same'),
+            tf.keras.layers.Conv2DTranspose(filters=inp_shape[2], kernel_size=3, activation='sigmoid', padding='same'),
             tf.keras.layers.Reshape(inp_shape)])
 
     def call(self, x):

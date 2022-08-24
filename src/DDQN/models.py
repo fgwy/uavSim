5a506a71158320c551445e6cc96ac7d5856485fd
fgwy <wyrwal.federico+github@gmail.com>
2022-08-24 16:51:42 +0200

    added simpler model

--- src/DDQN/models.py
+++ src/DDQN/models.py
@@ -130,6 +130,79 @@ def build_flat_model_masked(states_in, num_actions, initial_mb, path_to_local_pr
     return model
 
 
+def build_smaller_flat_model_masked(states_in, num_actions, initial_mb, path_to_local_pretrained_weights=None,
+                            name=''):  # local:17,17,4; global:21:21,4
+    '''
+     usage: model = build_hl_model(lm[tf.newaxis, ...], gm[tf.newaxis, ...], states_proc[tf.newaxis, ...])
+    '''
+
+    local_map_in, global_map_in, states_proc_in = states_in
+    # local_map_in_sg = tf.stop_gradient(local_map_in)
+    # global_map_in_sg = tf.stop_gradient(global_map_in)
+    # states_proc_in_sg = tf.stop_gradient(states_proc_in)
+
+    # states_proc = states_proc_in / initial_mb + 1e-6
+    states_proc = states_proc_in / 100
+
+    # local_map_model = build_lm_preproc_model(local_map_in, name)
+    # if path_to_local_pretrained_weights:
+    #     print(f'Loading weights from: {path_to_local_pretrained_weights}')
+    #     local_map_model.load_weights(path_to_local_pretrained_weights)
+    # flatten_local, local_map_2, local_map_3, local_map_4 = local_map_model.output
+
+    local_map = Conv2D(16, 5, activation='relu',
+                       strides=(1, 1),
+                       name=name + 'local_conv_' + str(0 + 1))(local_map_in)
+    local_map = Conv2D(16, 5, activation='relu',
+                       strides=(1, 1),
+                       name=name + 'local_conv_' + str(1 + 1))(local_map)
+
+    flatten_local = Flatten(name=name + 'local_flatten')(local_map)
+
+    # for k in range(2):
+    global_map = Conv2D(16, 5, activation='relu',
+                       strides=(1, 1),
+                       name=name + 'local_conv_' + str(0 + 1))(global_map_in)
+
+    global_map = Conv2D(16, 5, activation='relu',
+                       strides=(1, 1),
+                       name=name + 'local_conv_' + str(1 + 1))(global_map)
+
+    flatten_global = Flatten(name=name + 'local_flatten')(global_map)
+
+
+    flatten_map = tf.keras.layers.Concatenate(name=name + 'concat_flatten')(
+        [flatten_global, flatten_local, states_proc])
+
+    layer = Dense(256, activation='relu', name=name + 'hidden_layer_all_' + str(0))(
+        flatten_map)
+    layer = Dense(256, activation='relu', name=name + 'hidden_layer_all_' + str(0))(
+        layer)
+    layer = Dense(256, activation='relu', name=name + 'hidden_layer_all_' + str(0))(
+        layer)
+
+
+    Q_vals = tf.keras.layers.Dense(units=num_actions, activation=None, name=name + 'last_dense_layer_hl')(
+        layer)
+
+    # Masking of invalid actions
+
+    lz = 1-local_map_in[:, 8, 8, 2]  # negation of landing zone -> if no landing zone on position mask to -inf
+    b = tf.cast(tf.stack([local_map_in[:, 9, 8, 0],
+                          local_map_in[:, 8, 9, 0],
+                          local_map_in[:, 7, 8, 0],
+                          local_map_in[:, 8, 7, 0],
+                         lz], axis=-1), tf.bool)
+
+    # b = tf.cast((tf.keras.layers.Concatenate(axis=1)([a_1, c])), dtype=tf.bool)
+
+    Q_vals = tf.where(b, -np.inf, Q_vals)
+
+    print(f'Qvals: {Q_vals}')
+
+    model = tf.keras.Model(inputs=[local_map_in, global_map_in, states_proc_in], outputs=Q_vals)
+    return model
+
 def build_flat_model_no_mask(states_in, num_actions, initial_mb, path_to_local_pretrained_weights=None,
                              name=''):  # local:17,17,4; global:21:21,4
     '''
